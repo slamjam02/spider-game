bfe847ac2d28e6f077df0d335dc4a87ceee33804
Gabriel Que <gabeque11@gmail.com>
2025-05-15 18:59:46 -0400

    Updated Grapple Hook

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -11,11 +11,14 @@ public class GrapplingHook : MonoBehaviour
     [SerializeField] private LineRenderer rope;
 
     private Vector3 grapplePoint;
-    private DistanceJoint2D joint;
+    //private DistanceJoint2D joint;
+    private SpringJoint2D joint;
+    private Rigidbody2D player;
 
     void Start(){
         //disables joints and rope since we don't have a grapple point yet
-        joint = gameObject.GetComponent<DistanceJoint2D>();
+        //joint = gameObject.GetComponent<DistanceJoint2D>();
+        joint = gameObject.GetComponent<SpringJoint2D>();
         joint.enabled = false;
         rope.enabled = false;
     }
@@ -34,6 +37,7 @@ public class GrapplingHook : MonoBehaviour
             if(hit.collider != null){
                 grapplePoint = hit.point;
                 grapplePoint.z = 0;
+
                 joint.connectedAnchor = grapplePoint;
                 joint.enabled = true;
                 joint.distance = grappleLength;
@@ -44,10 +48,34 @@ public class GrapplingHook : MonoBehaviour
             }
         }
 
-        //When Left Click is released, disable joint and robe again
-        if(Input.GetMouseButtonUp(0)){
+        if(Input.GetMouseButtonDown(1)){
+            RaycastHit2D hit = Physics2D.Raycast(
+                origin: Camera.main.ScreenToWorldPoint(Input.mousePosition),
+                direction: Vector2.zero,
+                distance: Mathf.Infinity,
+                layerMask: grappleLayer
+            );
+
+            //If the Click is on a grapplable surface, set positions and enable joint and rope
+            if(hit.collider != null){
+                grapplePoint = hit.point;
+                grapplePoint.z = 0;
+
+                joint.connectedAnchor = grapplePoint;
+                joint.enabled = true;
+                joint.distance = 10;
+                
+                rope.SetPosition(0, grapplePoint);
+                rope.SetPosition(1, transform.position);
+                rope.enabled = true;
+            }
+        }
+
+        //When Left Click is released, disable joint and rope again
+        if(Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1)){
             joint.enabled = false;
             rope.enabled = false;
+            // player.AddForce
         }
 
         //Updates Rope Starting Pos to Track Player position
